94c6e938956bbf1e62b48c7a363fb2d77e465252
anhnvbk93 <anh.nguyen.it.dut@gmail.com>
2015-09-22 17:19:01 +0700

    [Update] Fix exersice mistake

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :microposts
-	validates FILL_IN, presense: true
-	validates FILL_IN, presense: true
+	validates :name, presense: true
+	validates :email, presense: true
 end
